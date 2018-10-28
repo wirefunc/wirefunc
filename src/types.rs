9d99537184a7eddd6bfd4274924d90fcb60b21cf
Richard Feldman <richard.t.feldman@gmail.com>
2018-10-28 11:28:26 -0400

    Tinker with types.

--- src/types.rs
+++ src/types.rs
@@ -1,31 +1,93 @@
 #[derive(Debug)]
-pub enum Tipe {
-    Int32,
-    Int64,
-    Int8,
-    Int16,
-    Float32,
-    Float64,
-    Float,
-    Int,
-    String,
-    Array(Box<Tipe>),
-    Dict(Box<Tipe>, Box<Tipe>),
-    Set(Box<Tipe>),
-    Record,
-    Custom,
+pub struct Type {
+    current: TypePrimitive,
+    formerly: Vec<TypePrimitive>,
 }
 
 #[derive(Debug)]
-pub enum Formerly {
-    Renamed(String),
-    Retyped(Tipe),
-    RenamedAndRetyped(String, Tipe),
+pub enum TypePrimitive {
+    Int,
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    Float,
+    Float32,
+    Float64,
+    String,
+    Array(Box<Type>),
+    Dict(Box<Type>, Box<Type>),
+    Set(Box<Type>),
+    Record(RecordInfo),
+    Custom(CustomTypeInfo),
+    Enum(EnumInfo),
+    Param(TypeParam),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Optionality {
+    Required,
+    Optional,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TypeParam {
+    TypeParam(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TypeName {
+    TypeName(String),
+}
+
+#[derive(Debug)]
+pub struct RecordInfo {
+    name: Option<TypeName>,
+    formerNames: Vec<TypeName>,
+    fields: Vec<Field>,
+    params: Vec<TypeParam>,
+}
+
+#[derive(Debug)]
+pub struct CustomTypeInfo {
+    name: TypeName,
+    formerNames: Vec<TypeName>,
+    variants: Vec<Variant>,
+    params: Vec<TypeParam>,
+}
+
+#[derive(Debug)]
+pub struct EnumInfo {
+    name: TypeName,
+    formerNames: Vec<TypeName>,
+    variants: Vec<EnumVariant>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct EnumVariant {
+    optionality: Optionality,
+    name: TypeName,
+    value: i32,
+    formerNames: Vec<String>,
 }
 
 #[derive(Debug)]
 pub struct Field {
-    tipe: Tipe,
+    optionality: Optionality,
+    name: String,
+    tipe: Type,
+    fieldId: i32,
     fallback: bool,
-    formerly: [Formerly],
+    formerNames: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Variant {
+    optionality: Optionality,
+    name: String,
+    tipe: Type,
+    variantId: i32,
+    fallback: bool,
+    formerNames: Vec<String>,
+    contents: Vec<Type>,
 }
