27036ab59e1aae2bbbdbe7aab642826a31670e45
Richard Feldman <richard.t.feldman@gmail.com>
2018-10-28 22:53:03 -0400

    Split out lib.rs and tests/

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+pub mod cli;
+pub mod code_gen;
+pub mod error_messages;
+pub mod field_id;
+pub mod language;
+pub mod problems;
+pub mod types;
+
+use problems::Problem;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+use std::path::PathBuf;
+
+fn report_problem(problem: Problem) {
+    // TODO color "Error:" in red.
+    eprintln!("\nError: {}", error_messages::report(problem));
+
+    std::process::exit(1);
+}
+
+pub fn run() -> () {
+    // Parse and validate CLI arguments
+    match cli::parse_args().map_err(Problem::Cli) {
+        Err(problem) => {
+            report_problem(problem);
+        }
+        Ok(args) => {
+            let _unique_file_paths: HashSet<PathBuf> = HashSet::from_iter(
+                args.file_paths
+                    .iter()
+                    .map(|file_path| file_path.to_path_buf()),
+            );
+
+            // Print the headline. Something like:
+            //
+            // wirefunc 0.1.0
+            // --------------
+            cli::print_headline();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,5 @@
-#[macro_use]
-extern crate maplit;
-
-mod cli;
-mod code_gen;
-mod error_messages;
-mod field_id;
-mod language;
-mod problems;
-mod types;
-
-use problems::Problem;
-use std::collections::HashSet;
-use std::iter::FromIterator;
-use std::path::PathBuf;
+extern crate wf;
 
 fn main() {
-    run().unwrap_or_else(report_problem);
-}
-
-fn report_problem(problem: Problem) {
-    // TODO color "Error:" in red.
-    eprintln!("\nError: {}", error_messages::report(problem));
-
-    std::process::exit(1);
-}
-
-fn run() -> Result<(), Problem> {
-    // Parse and validate CLI arguments
-    let args = cli::parse_args().map_err(Problem::Cli)?;
-
-    let _unique_file_paths: HashSet<PathBuf> = HashSet::from_iter(
-        args.file_paths
-            .iter()
-            .map(|file_path| file_path.to_path_buf()),
-    );
-
-    // Print the headline. Something like:
-    //
-    // wirefunc 0.1.0
-    // --------------
-    cli::print_headline();
-
-    Ok(())
+    wf::run();
 }
